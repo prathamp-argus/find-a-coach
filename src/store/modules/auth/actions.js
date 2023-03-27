867f46a6604fee9fa6b2edafb8dbdcc20d24acfb
pratham_prajapati <190320107095.ce.pratham.prajapati@gmail.com>
2023-03-27 12:35:48 +0530

    Final Project

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,84 @@
+let timer;
+
+export default {
+  async login(context, payload) {
+    return context.dispatch('auth', { ...payload, mode: 'login' })
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', { ...payload, mode: 'signup' })
+  },
+  async auth(context, payload) {
+    const mode = payload.mode
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMjVPteN8k-3ryviw5SclCUWCtcJb-Ei4'
+
+    if (mode === 'signup') {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBMjVPteN8k-3ryviw5SclCUWCtcJb-Ei4'
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
+      throw error;
+    }
+
+    const expiresIn = +responseData.expiresIn * 1000
+    const expirationDate = new Date().getTime() + expiresIn
+
+    timer = setTimeout(() => context.dispatch('autologout'), expiresIn)
+
+    localStorage.setItem('token', responseData.idToken)
+    localStorage.setItem('userId', responseData.localId)
+    localStorage.setItem('tokenExpiration', expirationDate)
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+    });
+  },
+  autologin(context) {
+    const token = localStorage.getItem('token')
+    const userId = localStorage.getItem('userId')
+    const tokenExpiration = localStorage.getItem('tokenExpiration')
+
+    const expiresIn = +tokenExpiration - new Date().getTime()
+
+    if (expiresIn < 10000) {
+      return;
+    }
+
+    timer = setTimeout(() => context.dispatch('autologout'), expiresIn)
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token,
+        userId
+      })
+    }
+  },
+  logout(context) {
+    localStorage.removeItem('token')
+    localStorage.removeItem('userId')
+    localStorage.removeItem('tokenExpiration')
+
+    context.commit('setUser', {
+      userId: null,
+      token: null,
+    })
+
+    clearTimeout(timer)
+  },
+  autologout(context) {
+    context.dispatch('logout')
+    context.commit('didLogout')
+  }
+};
